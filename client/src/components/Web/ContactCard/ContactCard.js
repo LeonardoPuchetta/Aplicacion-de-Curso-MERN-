63f36e2036ae7681fd61ae3b2c94ebf05f461c72
Leonardo Puchetta <rissottona@gmail.com>
2022-04-25 14:05:53 -0300

    seccion de contacto modificada

--- client/src/components/Web/ContactCard/ContactCard.js
+++ client/src/components/Web/ContactCard/ContactCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Row,Col,Card,Avatar} from 'antd';
 import Avatar2 from './../../../assets/img/png/Avatar2.png';
+import {GithubOutlined ,MailOutlined } from '@ant-design/icons';
 
 import './ContactCard.scss'
 
 
 export default function ContactCard() {
   return (
-    <div className='card-column'>
+    <div className='card-div'>
            <Col md={8} className='card-item'>
                             <CardReview 
                                 
@@ -15,26 +16,39 @@ export default function ContactCard() {
                                 subtitle=''
                                 avatar={Avatar2}
                                 review='leonardopuchetta21@gmail.com '
+                                icon={<MailOutlined/>}
                                 />
+                                <div className='link-contact'>
+                                    <button className='btn-contact'>
+                                    <a href='https://github.com/LeonardoPuchetta/Aplicacion-de-Curso-MERN-.git' target='_blank'>
+                                    <GithubOutlined spin='true' style={{ fontSize: '16px' }} className='icon'/>
+                                    Repositorio en GitHub</a>
+                                    </button>
+                                </div>
+                                
             </Col>
+            
     </div>
   )
 }
 function CardReview(props){
 
-    const {name ,subtitle,avatar, review} = props ; 
+    const {name ,subtitle,avatar, review,icon} = props ; 
     const {Meta} = Card ; 
 
     return(
 
-        <Card className='reviews-courses__card'>
+        <Card className='reviews-courses__card-contact'>
             
             <Meta
                 avatar={<Avatar src={avatar}/>}
                 title={name}
                 description={subtitle}
             />
-            <p>{review}</p>
+            <div className='reviews-courses__card-info'>
+            <p className='icon-contact'>{icon}</p>
+            <p >{review}</p>
+            </div>
             
         </Card>
     )
